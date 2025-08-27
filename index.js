083ab7ae7b09fad7dbd681d4a5e3ecc42b4d616e
sean boose <seanboose@gmail.com>
2025-08-26 20:13:03 -0700

    remove troubleshooting logging

--- index.js
+++ index.js
@@ -34,12 +34,6 @@ app.use(
 );
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(`CORS exception: ${process.env.CLIENT_ORIGIN}`);
-  console.log(`Request Origin: ${req.headers.origin}`);
-  next();
-});
-
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
